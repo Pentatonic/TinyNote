0eea32dd9bbf9d2fd5c4c47b0646871bcc3a2290
Pentatonic <john.pentatonic@gmail.com>
2011-02-22 19:37:29 +0800

    python api

---
+++ Python/class.py
@@ -0,0 +1,19 @@
+'''
+	Python class
+'''
+
+class Animal(object):
+	name = ''
+	
+	
+	def __init__(self, name):
+		self.name = name
+		
+	def getName(self):
+		return self.name
+	
+
+class Dog(Animal):
+	def __init__(self, name=''):
+		super(Dog, self).__init__(name)
+		print 'Create dog complete.'
---
+++ Python/socket.py
@@ -0,0 +1,42 @@
+'''
+	server
+	listen
+	socket
+	thread
+'''
+from threading import Thread
+import socket
+
+
+
+rcvSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+rcvSocket.bind(("", 12345))
+rcvSocket.listen(5)
+rcvMsgThread = Thread(target=handleRcvMsg)
+rcvMsgThread.start()
+
+
+def handleRcvMsg():
+	while True:
+#			print 'listening...'
+		clientSock, addr = rcvSocket.accept()
+		data = ""
+		while True:
+			buf = clientSock.recv(1024)
+			if not buf: break
+			data += buf
+#			print '[rcv]' + addr + ":" + data
+		clientSock.close()
+		
+		try:
+			cmdDict = json.loads(data)
+			
+			#
+			# Pop-up message
+			#
+			if cmdDict['cmd'] == 'jump':
+				data = cmdDict['data']
+			else:
+				continue
+		except:
+			pass
