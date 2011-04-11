9273fa49baa840a53d383348b1fbb396b472d122
Pentatonic <john.pentatonic@gmail.com>
2011-04-11 17:56:24 +0800

    lala

--- Java/web/MyServletContextListener.java
+++ Java/web/MyServletContextListener.java
@@ -12,34 +12,34 @@ Therefore, ServletContextListner is helpful in informing about context
 Initilazation and destruction.
 */
 
-public	class MyListener implements ServletContextListener {
+public    class MyListener implements ServletContextListener {
 
-	private ServletContext context = null;
-
-
-	public void contextDestroyed(ServletContextEvent event){
-		//Output a simple message to the server's console
-		System.out.println("The Simple Web App. Has Been Removed");
-		this.context = null;
-
-	}
+    private ServletContext context = null;
 
 
-	public void contextInitialized(ServletContextEvent event){
-		this.context = event.getServletContext();
+    public void contextDestroyed(ServletContextEvent event){
+        //Output a simple message to the server's console
+        System.out.println("The Simple Web App. Has Been Removed");
+        this.context = null;
 
-		//Output a simple message to the server's console
-		System.out.println("The Simple Web App. Is Ready");
+    }
 
-	}
+
+    public void contextInitialized(ServletContextEvent event){
+        this.context = event.getServletContext();
+
+        //Output a simple message to the server's console
+        System.out.println("The Simple Web App. Is Ready");
+
+    }
 }
 
 <web-app>
-	<listener>
-		<listener-class>
-			com.listeners.MyContextListener
-		</listener-class>
-	</listener>
-	<servlet/>
-	<servlet-mapping/>
+    <listener>
+        <listener-class>
+            com.listeners.MyContextListener
+        </listener-class>
+    </listener>
+    <servlet/>
+    <servlet-mapping/>
 </web-app> 
