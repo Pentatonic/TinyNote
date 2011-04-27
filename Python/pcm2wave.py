450f186295a28fb1d45ef31d67971efa4101a782
Pentatonic <john.pentatonic@gmail.com>
2011-04-27 14:22:56 +0800

    pcm to wave

--- Python/pcm2wave.py
+++ Python/pcm2wave.py
@@ -5,7 +5,9 @@
 import array
 from struct import *
 import os
-fname = './VOICE.pcm'
+import math
+
+fname = './VOICE1.pcm'
 pcmSize = os.path.getsize(fname)
 print 'pcm size:%s' % pcmSize
 
@@ -25,10 +27,18 @@ f.write('data')
 f.write(pack('I', pcmSize)) # size of data chunk
 
 # The 16-bit PCM file is in Motorolla PCM format
-# which is big-endian, wave files use little-endian
+# which is big-endian, wave file's data use little-endian
 data = open(fname, 'rb').read()
-big_end = unpack('>%s' % ('H' * (pcmSize / 2)), data)
-for b in big_end:
-    f.write(pack('H', b))
+big_end = unpack('>%s' % ('h' * (pcmSize / 2)), data)
+for i in xrange(len(big_end)):
+    b_loud = big_end[i]# * 8
+    if b_loud > 32767:
+        b_loud = 32767
+    elif b_loud < -32768:
+        b_loud = -32768
+    #if math.fabs(b_loud) < 3000 and i > 0:
+    #if i > 0 and i < len(big_end) - 1:
+    #    b_loud = (big_end[i-1] + b_loud + big_end[i+1]) / 3
+    f.write(pack('h', b_loud))
 f.close()
 
