f984e6084afcc95e4375b7ed3995ee6675c925a0
john.liu <john.pentatonic@gmail.com>
2018-03-28 11:12:34 +0800

    add ggplot note

--- Python/DataScience/ggplot_sample.py
+++ Python/DataScience/ggplot_sample.py
@@ -1,9 +1,38 @@
 from pandas import *
 from ggplot import *
 
-df = pandas.read_csv('hr_year.csv')
-gp = ggplot(df, aes(x='yearID', y='HR')) + geom_point(color='red') + geom_line(color='red') \
-+ ggtitle('HR by year') + xlab('year') + ylab('HR')
+def lineplot(csv):
+    df = pandas.read_csv(csv)
+    gp = ggplot(df, aes(x='yearID', y='HR'))
+    gp += geom_point(color='red') + geom_line(color='red')
+    gp += ggtitle('HR by year') + xlab('year') + ylab('HR')
+    return gp
 
-gp.show()
+def lineplot_compare(hr_by_team_year_sf_la_csv):
+    # Write a function, lineplot_compare, that will read a csv file
+    # called hr_by_team_year_sf_la.csv and plot it using pandas and ggplot.
+    #
+    # This csv file has three columns: yearID, HR, and teamID. The data in the
+    # file gives the total number of home runs hit each year by the SF Giants 
+    # (teamID == 'SFN') and the LA Dodgers (teamID == "LAN"). Produce a 
+    # visualization comparing the total home runs by year of the two teams. 
+    #
+    # You can see the data in hr_by_team_year_sf_la_csv
+    # at the link below:
+    # https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/hr_by_team_year_sf_la.csv
+    #
+    # Note that to differentiate between multiple categories on the 
+    # same plot in ggplot, we can pass color in with the other arguments
+    # to aes, rather than in our geometry functions. For example, 
+    # ggplot(data, aes(xvar, yvar, color=category_var)). This might help you 
+    # in this exercise.
+
+    df = pandas.read_csv(hr_by_team_year_sf_la_csv)
+    gg = ggplot(df, aes(x='yearID', y='HR', color='teamID')) \
+        + geom_point() + geom_line() \
+        + ggtitle('HR by year') + xlab('year') + ylab('HR')
+    return gg
+
+print lineplot('hr_year.csv')
+print lineplot_compare('hr_by_team_year_sf_la.csv')
 
