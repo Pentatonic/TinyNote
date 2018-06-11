23681a32c15fa8b1776ad675f2d51be31c3b0cd1
pentatonic <john.pentatonic@gmail.com>
2018-06-11 11:27:46 +0800

    Add note

---
+++ C_CPP/trace.cpp
@@ -0,0 +1,28 @@
+#include<string>
+#include<iostream>
+#include<vector>
+
+using namespace::std;
+// C++11
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char *name, Arg1&& arg1) {
+    cerr << name << ": " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char *names, Arg1&& arg1, Args&&... args) {
+    const char *comma = strchr(names + 1, ',');
+    cerr.write(names, comma - names) << ": " << arg1 << " |";
+    __f(comma + 1, args...);
+}
+
+int main() {
+    int a = 123;
+    int b = 456;
+    int c = 789;
+    trace(a);
+    trace(a, b, c);
+    trace('1');
+    trace("1");
+    trace(1);
+}
